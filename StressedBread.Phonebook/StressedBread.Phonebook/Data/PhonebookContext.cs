3da4f45efa21285b7102122e73b2aaeaf04f0aab
StressedBread <martinpolak02@gmail.com>
2026-05-28 17:28:15 +0200

    feat(DB): add EF Core setup with SQL Server provider
    Introduce EF Core with SQL Server, DbContext, and migrations.

---
+++ StressedBread.Phonebook/StressedBread.Phonebook/Data/PhonebookContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StressedBread.Phonebook.Data;
+public class PhonebookContext : DbContext
+{ 
+    public PhonebookContext(DbContextOptions<PhonebookContext> options) : base(options)
+    {
+    }
+}
--- StressedBread.Phonebook/StressedBread.Phonebook/Program.cs
+++ StressedBread.Phonebook/StressedBread.Phonebook/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StressedBread.Phonebook.Data;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddDbContext<PhonebookContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<PhonebookContext>();
+    context.Database.Migrate();
+}
