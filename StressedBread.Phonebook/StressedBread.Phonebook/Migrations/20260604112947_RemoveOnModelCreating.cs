5bf0adc44ce1f3a16d038aa3c9ac368cf9f5bee4
StressedBread <martinpolak02@gmail.com>
2026-06-04 13:32:39 +0200

    chore(DB): OnModelCreating migration files

---
+++ StressedBread.Phonebook/StressedBread.Phonebook/Migrations/20260604112947_RemoveOnModelCreating.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StressedBread.Phonebook.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveOnModelCreating : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
