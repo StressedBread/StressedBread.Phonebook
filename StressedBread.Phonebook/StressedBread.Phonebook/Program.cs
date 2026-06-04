d213bede63783a448d7a5da3da8be0d554fcf16a
StressedBread <martinpolak02@gmail.com>
2026-06-04 15:20:25 +0200

    feat(Validation): add connection string validation
    Introduce connection string validation to ensure proper configuration. Add error handling for database connectivity and migration issues.

--- StressedBread.Phonebook/StressedBread.Phonebook/Program.cs
+++ StressedBread.Phonebook/StressedBread.Phonebook/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using PhoneNumbers;
+using Spectre.Console;
 using StressedBread.Phonebook.Controllers;
 using StressedBread.Phonebook.Data;
 using StressedBread.Phonebook.Services;
@@ -10,6 +11,16 @@ using StressedBread.Phonebook.UI;
 using StressedBread.Phonebook.Validation;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+var (isValid, message) = ConnectionStringCheck.IsValidConnectionString(connectionString);
+
+if (!isValid)
+{
+    AnsiConsole.MarkupLine($"[red]{message}[/]");
+    return;
+}
 
 builder.Services.AddDbContext<PhonebookContext>(options =>
 {
@@ -26,7 +37,19 @@ var app = builder.Build();
 
 using var scope = app.Services.CreateScope();
 var context = scope.ServiceProvider.GetRequiredService<PhonebookContext>();
-context.Database.Migrate();
-
 var controller = scope.ServiceProvider.GetRequiredService<PhonebookController>();
-await controller.Run();
+
+try
+{
+    if (await context.Database.CanConnectAsync())
+    {
+        context.Database.Migrate();
+        await controller.Run();
+    }
+    else
+        AnsiConsole.MarkupLine("[red]Unable to connect to the database.[/]");
+}
+catch (Exception ex)
+{
+    AnsiConsole.MarkupLine($"[red]An error occurred: {ex.Message}[/]");
+}
---
+++ StressedBread.Phonebook/StressedBread.Phonebook/Validation/ConnectionStringCheck.cs
@@ -0,0 +1,10 @@
+﻿namespace StressedBread.Phonebook.Validation;
+public class ConnectionStringCheck
+{
+    public static (bool IsValid, string Message) IsValidConnectionString(string? connectionString)
+    {
+        if (string.IsNullOrEmpty(connectionString))
+            return (false, "Connection string is missing. Please check your configuration.");
+        return (true, "");
+    }
+}
